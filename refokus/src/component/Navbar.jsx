6eb866f8131c7095286878e005998112fc6e70fb
Md Shahnawaz <mdshahnawaz.420bca@gmail.com>
2025-09-29 09:34:29 +0000

    " "

--- refokus/src/component/Navbar.jsx
+++ refokus/src/component/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
       <div className="links flex gap-10">
         {["Home", "Work", 'Culture',"","News"].map((item,index) =>( 
           item.length === 0 ? <span className='h-6 w-0.5 bg-zinc-600'></span>:<a className='flex items-center gap-2 text-sm' href='#'>
-         {index === 1 ? (<span style={{boxShadow:" 0 0 0.45em #00FF19"}} className='inline-block w-2 h-2 rounded-full  bg-green-600'></span>):null} 
+         {index === 1 ? (<span  style={{boxShadow:" 0 0 0.45em #00FF19"}} className='inline-block w-2 h-2 rounded-full  bg-green-600'></span>):null} 
           {item}</a>
         ))}
           
